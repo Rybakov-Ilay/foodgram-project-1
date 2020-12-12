092426df4396b7244c9b6897af975375fb2ee585
dementevm <dem.90@list.ru>
2020-12-12 22:02:04 +0300

    deploy fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ echo 'collecting static'
 python manage.py collectstatic --noinput
 
 echo 'running server'
-gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
